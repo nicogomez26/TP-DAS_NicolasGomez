ddb21f779f870d21eeb662e12a079835451cc51d
NicolasGomez <nicolasgomez1458@gmail.com>
2025-12-16 14:07:46 -0300

    .

--- TP-DAS/TP-DAS/Login.Designer.cs
+++ TP-DAS/TP-DAS/Login.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(705, 64);
+            this.button1.Location = new System.Drawing.Point(691, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(217, 39);
             this.button1.TabIndex = 5;
